d391f7fd1dfef8584cfee5b42b4aca75d080703c
alexmeinhold <alexander.meinhold@outlook.com>
2019-07-01 00:43:13 +0200

    prevent submitting empty form, shorten long filenames

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,9 @@
 const uploadButton = document.querySelector('#upload-button')
 const realInput = document.querySelector('#file-upload')
 const fileInfo = document.querySelector('#file-info')
+const classifyButton = document.querySelector('#classify')
+
+const MAX_LENGTH = 25
 
 uploadButton.addEventListener('click', () => {
   realInput.click()
@@ -8,7 +11,14 @@ uploadButton.addEventListener('click', () => {
 
 realInput.addEventListener('change', () => {
   if (realInput.value) {
-    fileInfo.innerHTML = realInput.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+    fileName = realInput.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+
+    if (fileName.length > 25) {
+        fileName = fileName.slice(0, 10) + '...' + fileName.slice(-11)
+    }
+
+    fileInfo.innerHTML = fileName
+    classifyButton.disabled = false
   } else {
     fileInfo.innerHTML = 'No file chosen'
   }
